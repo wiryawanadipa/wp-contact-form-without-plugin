1cc36d8f70de96e216d472a7f030d978889fbc25
wiryawanadipa <wiryawanadipa.trityavijaya@gmail.com>
2022-09-07 10:19:01 +0700

    add sanitize

--- contact-form.php
+++ contact-form.php
@@ -10,41 +10,37 @@
 <?php
 if(isset($_POST['submit'])) {
 	if ($_SESSION['rand'] == $_POST['randcheck']) {
-		if(trim($_POST['contactName']) === '') {
+		if(sanitize_text_field($_POST['contactName']) === '') {
 			$emptyNameError = true;
-		} else if (strlen(trim($_POST['contactName'])) > 50) {
+		} else if (strlen(sanitize_text_field($_POST['contactName'])) > 50) {
 			$longNameError = true;
 		} else {
-			$name = trim($_POST['contactName']);
+			$name = sanitize_text_field($_POST['contactName']);
 		}
-		if(trim($_POST['email']) === '')  {
+		if(sanitize_text_field($_POST['email']) === '')  {
 			$emptyEmailError = true;
-		} else if (strlen(trim($_POST['email'])) > 80) {
+		} else if (strlen(sanitize_text_field($_POST['email'])) > 80) {
 			$longEmailError = true;
-		} else if (!preg_match("/^[[:alnum:]][a-z0-9_.-]*@[a-z0-9.-]+\.[a-z]{2,6}$/i", trim($_POST['email']))) {
+		} else if (!preg_match("/^[[:alnum:]][a-z0-9_.-]*@[a-z0-9.-]+\.[a-z]{2,6}$/i", sanitize_text_field($_POST['email']))) {
 			$invalidEmailError = true;
 		} else {
-			$email = trim($_POST['email']);
+			$email = sanitize_text_field($_POST['email']);
 		}
-		if(trim($_POST['message']) === '') {
+		if(sanitize_textarea_field($_POST['message']) === '') {
 			$emptyMessageError = true;
-		} else if (strlen(trim($_POST['message'])) > 1000) {
+		} else if (strlen(sanitize_textarea_field($_POST['message'])) > 1000) {
 			$longMessageError = true;
 		} else {
-			if(function_exists('stripslashes')) {
-				$message = stripslashes(trim($_POST['message']));
-			} else {
-				$message = trim($_POST['message']);
-			}   
+			$message = sanitize_textarea_field($_POST['message']);
 		}
-		if(!isset($emptyNameError) && strlen(trim($_POST['contactName'])) < 51 && !isset($emptyEmailError) && strlen(trim($_POST['email'])) < 81 && !isset($invalidEmailError) && !isset($emptyMessageError) && strlen(trim($_POST['message'])) < 1001 && !empty($_POST['g-recaptcha-response'])) {
-			$secret = 'YOUR_SECRET_KEY'; // Change it to your reCaptcha secret key
+		if(!isset($emptyNameError) && strlen(sanitize_text_field($_POST['contactName'])) < 51 && !isset($emptyEmailError) && strlen(sanitize_text_field($_POST['email'])) < 81 && !isset($invalidEmailError) && !isset($emptyMessageError) && strlen(sanitize_textarea_field($_POST['message'])) < 1001 && !empty($_POST['g-recaptcha-response'])) {
+			$secret = 'SECRET_KEY';
 			$ip = $_SERVER['REMOTE_ADDR'];
 			$captcha = $_POST['g-recaptcha-response'];
 			$rsp = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret=' . $secret . '&response=' . $captcha .'&remoteip='. $ip);
 			$valid = json_decode($rsp, true);
 			if($valid["success"] == true) {
-				$emailTo = 'YOUR_EMAIL'; // Change it to your email address
+				$emailTo = 'YOUR_EMAIL';
 				$domain = strtoupper($_SERVER['HTTP_HOST']);
 				$subject = '[' . $domain . '] From ' . $name;
 				$body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Message: ' . $message;
@@ -98,21 +94,21 @@ if(isset($_POST['submit'])) {
 				<div class="row mb-0 mb-md-3">
 					<div class="col-12 col-md-6 mb-3 mb-md-0">
 						<label class="mb-2">Name<span>&#42;</span></label>
-						<input class="form-control" name="contactName" type="text" placeholder="Please enter your name here." maxlength="50" value="<?php if( isset( $_POST['contactName'] ) && !isset($emailSent) ) { echo esc_attr($_POST['contactName']);} else { echo '';} ?>" autofocus required>
+						<input class="form-control" name="contactName" type="text" placeholder="Please enter your name here." maxlength="50" value="<?php if( isset( $_POST['contactName'] ) && !isset($emailSent) ) { echo sanitize_text_field($_POST['contactName']);} else { echo '';} ?>" autofocus required>
 					</div>
 					<div class="col-12 col-md-6 mb-3 mb-md-0">
 						<label class="mb-2">E-Mail<span>&#42;</span></label>
-						<input class="form-control" name="email" type="email" placeholder="Please enter your e-mail address here." maxlength="80" value="<?php if( isset( $_POST['email'] ) && !isset($emailSent) ) { echo esc_attr($_POST['email']);} else { echo '';} ?>" required>
+						<input class="form-control" name="email" type="email" placeholder="Please enter your e-mail address here." maxlength="80" value="<?php if( isset( $_POST['email'] ) && !isset($emailSent) ) { echo sanitize_text_field($_POST['email']);} else { echo '';} ?>" required>
 					</div>
 				</div>
 				<div class="row mb-3">
 					<div class="col-12">
 						<label class="mb-2">Message<span>&#42;</span></label>
-						<textarea id="message" class="form-control" placeholder="Please enter your message here." maxlength="1000" name="message" rows="10" required><?php if( isset( $_POST['message'] ) && !isset($emailSent) ) { echo esc_attr($_POST['message']);} else { echo '';} ?></textarea>
+						<textarea id="message" class="form-control" placeholder="Please enter your message here." maxlength="1000" name="message" rows="10" required><?php if( isset( $_POST['message'] ) && !isset($emailSent) ) { echo sanitize_textarea_field($_POST['message']);} else { echo '';} ?></textarea>
 					</div>
 				</div>
 				<div id="messagecharcounter" class="mb-3 text-end text-light">
-					<span id="typedchar"><?php if( isset( $_POST['message'] ) && !isset($emailSent) ) { echo strlen(trim($_POST['message']));} else { echo '0';} ?></span>
+					<span id="typedchar"><?php if( isset( $_POST['message'] ) && !isset($emailSent) ) { echo strlen(sanitize_textarea_field($_POST['message']));} else { echo '0';} ?></span>
 					<span id="maxchar">/ 1000</span>
 				</div>
 				<script>
@@ -137,7 +133,7 @@ if(isset($_POST['submit'])) {
 				</script>
 				<div class="row mb-4">
 					<div class="col-12 col-md-6 mb-4 mb-md-0">
-						<div class="g-recaptcha brochure__form__captcha" data-sitekey="YOUR_SITE_KEY"></div> <!-- Change it to your reCaptcha secret key -->
+						<div class="g-recaptcha brochure__form__captcha" data-sitekey="SITE_KEY"></div>
 					</div>
 					<div class="col-12 col-md-6 mb-4 mb-md-0 text-start text-md-end">
 						<?php 
